b7cf008bc3d8422aee1a75c66680945f51c0e81a
looser88 <98893135+looser88@users.noreply.github.com>
2023-02-23 23:36:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ from Script import script
 from datetime import date, datetime 
 import pytz
 from aiohttp import web
-from plugins import web_server
+from plugins import we
 
 class Bot(Client):
 
