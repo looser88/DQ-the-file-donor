5d1ee5a06f9a1cac02d5a8aeb2ef10410085e277
looser88 <98893135+looser88@users.noreply.github.com>
2023-02-24 00:26:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
-from pyrogram import Client, __version__
+
+from pyrogram import Client, __version__, filters
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
@@ -61,7 +62,7 @@ class Bot(Client):
     async def stop(self, *args):
         await super().stop()
         logging.info("Bot stopped. Bye.")
-
+    
     async def iter_messages(
         self,
         chat_id: Union[int, str],
@@ -100,6 +101,8 @@ class Bot(Client):
             for message in messages:
                 yield message
                 current += 1
+
+
 
 
 app = Bot()
