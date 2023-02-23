2c7a68f96b34aff553830f77b517b5342c72a327
looser88 <98893135+looser88@users.noreply.github.com>
2023-02-23 23:53:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 import logging.config
 
 from aiohttp import web
-from aiohttp import web_server
+from plugins import web_server
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
